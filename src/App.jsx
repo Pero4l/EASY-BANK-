e23b222c551af8172942588207c37d4a740666b3
Peter Abahim <lilpero03@gmail.com>
2025-06-20 16:06:35 +0100

    i just setup the working space and about to start working

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+      <div>
+        <h1 className='bg-red-50'>helow</h1>
+      </div>
+  )
+}
+
+export default App
